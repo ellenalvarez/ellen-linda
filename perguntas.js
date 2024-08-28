adc17357dc9d0be3bc18511007e03fbf62999c2e
ellenalvarez <ellen.alvarez@escola.pr.gov.br>
2024-08-28 07:46:09 -0300

    novo projeto

---
+++ perguntas.js
@@ -0,0 +1,69 @@
+export const perguntas = [
+    {
+        enunciado: "Você está saindo da escola, e se depara com uma enchente iminente, VOCÊ É O ÚNICO QUE PODE SALVAR O RIO GRANDE DO SUL.",
+        alternativas: [
+            {
+                texto: "Será que eu consigo?",
+                afirmacao: [
+                    "No início se sentiu amedrontrado.",
+                    "Achou assustador ver que você pode ser o herói dessa história"
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Vamos salvá-los!",
+                afirmacao: [
+                    "Se colocou a disposição não importando oque acontecesse.",
+                    "Não hesitou em ajudar."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Você se recorda que tem vários lugares que são para as pessoas adotarem animais de estimação, você iria ajudar os animais ?",
+        alternativas: [
+            {
+                texto: "Tenho mais coisas para me preocupar, esses animais em sua maioria sabem nadar, vão conseguir sozinhos !",
+                afirmacao: [
+                    "Não ligou para os animais, colocou a sua pessoa no primeiro plano, e nem cogitou salvá-los depois.",
+                    "Pensou que tinha coisas melhores para serem feitas, como fazer batata frita.",
+                    "Foi embora para muito longe, esquecendo de tudo e todos."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Irei de van para salvá-los, assim poderei o máximo de animais de uma só vez !",
+                afirmacao: [
+                    "Mesmo estando sozinho, resolveu ajudar o máximo que pudesse.",
+                    "Vamoooooooo, salvar esses animais !",
+                    "Coloco uma música no meu fone, e continuo a minha jornada em salvar os pets"
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Passou se um tempo, e agora tudo está normal e pedem para que você faça um pronunciamento.",
+        alternativas: [
+            {
+                texto: "Foi muito bom ajudar essas pessoas, acredito que isso tenha prevenido mortes, doenças e quaisquer outras coisas ruins.",
+                afirmacao: [
+                    "Ótimo pronunciamento, todos te aplaudem, Parabéns senho",
+                    "A multidão vai à loucura e todos te veneram, yeahhhhhhhh!!!",
+                    "Você fala poucas palavras, e tem uma saída triunfal"
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Ah eu queria receber um prêmio em dinheiro por ter salvo essas pessoas.",
+                afirmacao: [
+                    "Mentiroso, você não salvou ninguém, apenas pensou em você.",
+                    "Porque está mentindo sendo que você não fez nada ?",
+                    "Matuê te desmentiu, que desonra !"
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+];
